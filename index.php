618460c94a9a5382379f1e42b9d95e60e51f46f0
mlietti1 <mcf.lietti@outlook.com>
2022-12-02 17:12:21 +0100

    funzione pw mista

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
 
+
+
 $numbers = '0123456789';
 $lc_letters = 'abcdefghijklmnopqrstuvwxyz';
 $uc_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 $symbols = '!?&%$<>^+-*/()[]{}@#_=';
+
+// $randomNum = $numbers[rand(0, strlen($numbers) - 1)];
+// $randomLcL = $lc_letters[rand(0, strlen($lc_letters) - 1)];
+// $randomUcL = $uc_letters[rand(0, strlen($uc_letters) - 1)];
+// $randomSym = $symbols[rand(0, strlen($symbols) - 1)];
+
+
+$length = $_GET['length'];
+
+function createPassword($length)
+{
+  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?&%$<>^+-*/()[]{}@#_=';
+  $password = '';
+  for ($i = 0; $i < $length; $i++) {
+    $password .= $characters[rand(0, strlen($characters) - 1)];
+  }
+  echo $password;
+}
+
+
 
 ?>
 
@@ -30,6 +52,8 @@ $symbols = '!?&%$<>^+-*/()[]{}@#_=';
           <button class="btn btn-primary" type="submit">Create</button>
         </form>
       </div>
+
+      <p>Your password: </p><?php createPassword($length)  ?>
     </div>
 
   </div>
